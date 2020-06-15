b65e0cd01e850acf49a29f421911b70da4f6133a
Mori01231 <mori01231mc@gmail.com>
2020-06-15 22:19:34 +0900

    Adapted for us in LGW

--- src/main/java/com/github/mori01231/lifecore/SwitchCommandExecutor.java
+++ src/main/java/com/github/mori01231/lifecore/SwitchCommandExecutor.java
@@ -12,13 +12,17 @@ import static org.bukkit.Bukkit.getServer;
 public class SwitchCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        String servername = LifeCore.getInstance().getConfig().getString("lp-server-name");
+
+
         if (sender instanceof Player){
             Player player = (Player) sender;
 
             //NO PERMISSION
 
-            if(!(sender.hasPermission("lifecore.switchadmin") || sender.hasPermission("lifecore.switchdeveloper") || sender.hasPermission("lifecore.switchowner")
-                    || sender.hasPermission("lifecore.isadmin") || sender.hasPermission("lifecore.isdeveloper") || sender.hasPermission("lifecore.isowner"))){
+            if(!(sender.hasPermission("lifecore.switchmoderator") || sender.hasPermission("lifecore.switchadminmember")|| sender.hasPermission("lifecore.switchadmin") || sender.hasPermission("lifecore.switchdeveloper") || sender.hasPermission("lifecore.switchowner")
+                    || sender.hasPermission("lifecore.ismoderator") || sender.hasPermission("lifecore.isadminmember") || sender.hasPermission("lifecore.isadmin") || sender.hasPermission("lifecore.isdeveloper") || sender.hasPermission("lifecore.isowner"))){
 
                 sender.sendMessage(ChatColor.translateAlternateColorCodes
                         ('&',"&4権限不足です。\n&3Mori01231#9559のDMにこのメッセージのスクショをもって泣きつきましょう！" ));
@@ -27,12 +31,28 @@ public class SwitchCommandExecutor implements CommandExecutor {
 
             //CHANGE TO ADMIN MODE
 
+            //member to moderator
+            if (sender.hasPermission("lifecore.switchmoderator") && !sender.hasPermission("lifecore.ismoderator")){
+
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add moderator server=" + servername);
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3Moderatorモードになりました。" ));
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove switchmoderator server=" + servername);
+            }
+
             //member to admin
             if (sender.hasPermission("lifecore.switchadmin") && !sender.hasPermission("lifecore.isadmin")){
 
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add admin server=life");
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add admin server=" + servername);
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3Adminモードになりました。" ));
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove switchadmin");
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove switchadmin server=" + servername);
+            }
+
+            //member to adminmember
+            if (sender.hasPermission("lifecore.switchadminmember") && !sender.hasPermission("lifecore.isadminmember")){
+
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add adminmember server=" + servername);
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3AdminMemberモードになりました。" ));
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove switchadminmember server=" + servername);
             }
 
             //member to developer
@@ -57,12 +77,31 @@ public class SwitchCommandExecutor implements CommandExecutor {
 
             //CHANGE TO MEMBER MODE
 
+
+            //moderator to member
+            if (sender.hasPermission("lifecore.ismoderator")){
+
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add switchmoderator server=" + servername);
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3Memberモードになりました。" ));
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove moderator server=" + servername);
+
+            }
+
             //admin to member
             if (sender.hasPermission("lifecore.isadmin")){
 
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add switchadmin");
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add switchadmin server=" + servername);
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3Memberモードになりました。" ));
-                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove admin server=life");
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove admin server=" + servername);
+
+            }
+
+            //adminmember to member
+            if (sender.hasPermission("lifecore.isadminmember")){
+
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent add switchadminmember server=" + servername);
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3Memberモードになりました。" ));
+                getServer().dispatchCommand(getServer().getConsoleSender(), "lp u " + player.getName() + " parent remove adminmember server=" + servername);
 
             }
 
