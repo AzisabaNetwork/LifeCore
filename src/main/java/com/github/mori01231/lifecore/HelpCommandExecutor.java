c0f6f1f012b583607f5ff456ca6f0de3adc762a3
Mori01231 <mori01231mc@gmail.com>
2020-06-18 23:17:59 +0900

    [New] Added help for Town in default config.yml

--- src/main/java/com/github/mori01231/lifecore/HelpCommandExecutor.java
+++ src/main/java/com/github/mori01231/lifecore/HelpCommandExecutor.java
@@ -10,10 +10,23 @@ public class HelpCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-
-        for (String line : LifeCore.getInstance().getConfig().getStringList("Help.Menu")) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
+        if(args.length == 1){
+            if (args[0].equalsIgnoreCase("menu")) {
+                for (String line : LifeCore.getInstance().getConfig().getStringList("Help.Menu")) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
+                }
+            }
+            else if (args[0].equalsIgnoreCase("official")) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3公式Wikiリンク: &f&l" + LifeCore.getInstance().getConfig().getString("wiki-url") ));
+            }
         }
+
+        else{
+            for (String line : LifeCore.getInstance().getConfig().getStringList("Help.Menu")) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
+            }
+        }
+
 
         return true;
     }
