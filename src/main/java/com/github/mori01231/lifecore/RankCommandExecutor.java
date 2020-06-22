7adcd7fa433c8deb1de615ea86e52088bcf0bcfc
Mori01231 <mori01231mc@gmail.com>
2020-06-23 03:28:20 +0900

    [Fix] Reformat redundant code.

--- src/main/java/com/github/mori01231/lifecore/RankCommandExecutor.java
+++ src/main/java/com/github/mori01231/lifecore/RankCommandExecutor.java
@@ -13,23 +13,19 @@ public class RankCommandExecutor implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if(args.length == 1){
-            if(args[0].length() == 1){
-                if (args[0].charAt(0) < '0' || args[0].charAt(0) > '9') {
-                    for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank" + args[0])) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
-                    }
-                }
-                else{
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a/rank &3の後に1~10の数字を入れてください。例：&a/rank 3"));
-                }
-            }
 
-            else if (args[0].length() == 2 && args[0].charAt(0) == '1' && args[0].charAt(0) == '0'){
-                for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank" + args[0])) {
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
-                }
+            int foo = 0;
+            try {
+                foo = Integer.parseInt(args[0]);
             }
-            else{
+            catch (NumberFormatException e)
+            {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a/rank &3の後に1~10の数字を入れてください。例：&a/rank 3"));
+            }
+            for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank" + args[0])) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', line));
+            }
+            if(!(foo <= 10 && foo >= 1)){
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&a/rank &3の後に1~10の数字を入れてください。例：&a/rank 3"));
             }
 
@@ -37,74 +33,15 @@ public class RankCommandExecutor implements CommandExecutor {
 
         else{
             if (sender instanceof Player){
-
-                if(sender.hasPermission("lifecore.rank10")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3現在のランク：&b&lランク10"));
-                    for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank10")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
+                for (int i = 10; i >= 1; i--) {
+                    if(sender.hasPermission("lifecore.rank" + i)){
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3現在のランク：&b&lランク" + i));
+                        for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank" + i)) {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
+                        }
+                        break;
                     }
                 }
-                else if(sender.hasPermission("lifecore.rank9")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3現在のランク：&b&lランク9"));
-                    for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank9")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
-                    }
-                }
-                else if(sender.hasPermission("lifecore.rank8")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3現在のランク：&b&lランク8"));
-                    for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank8")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
-                    }
-                }
-                else if(sender.hasPermission("lifecore.rank7")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3現在のランク：&b&lランク7"));
-                    for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank7")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
-                    }
-                }
-                else if(sender.hasPermission("lifecore.rank6")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3現在のランク：&b&lランク6"));
-                    for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank6")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
-                    }
-                }
-                else if(sender.hasPermission("lifecore.rank5")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3現在のランク：&b&lランク5"));
-                    for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank5")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
-                    }
-                }
-                else if(sender.hasPermission("lifecore.rank4")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3現在のランク：&b&lランク4"));
-                    for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank4")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
-                    }
-                }
-                else if(sender.hasPermission("lifecore.rank3")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3現在のランク：&b&lランク3"));
-                    for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank3")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
-                    }
-                }
-                else if(sender.hasPermission("lifecore.rank2")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3現在のランク：&b&lランク2"));
-                    for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank2")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
-                    }
-                }
-                else if(sender.hasPermission("lifecore.rank1")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3現在のランク：&b&lランク1"));
-                    for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Rank1")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
-                    }
-                }
-                else if(sender.hasPermission("lifecore.member")){
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3現在のランク：&b&lMember"));
-                    for (String line : LifeCore.getInstance().getConfig().getStringList("Rank.Member")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',line));
-                    }
-                }
-
             }
             else{
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3このコマンドはコンソールから使用できません。" ));
