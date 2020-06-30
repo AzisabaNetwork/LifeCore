5946ddeea07441481966e5a4ebf744bce1513376
Mori01231 <mori01231mc@gmail.com>
2020-06-30 23:37:38 +0900

    [Add] /trash now opens inventory

--- src/main/java/com/github/mori01231/lifecore/TrashCommandExecutor.java
+++ src/main/java/com/github/mori01231/lifecore/TrashCommandExecutor.java
@@ -1,12 +1,34 @@
 package com.github.mori01231.lifecore;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
 
 public class TrashCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+
+        if ((sender instanceof Player))
+        {
+            Player p = (Player)sender;
+
+            // Here we create our named help "inventory"
+            Inventory trash = Bukkit.getServer().createInventory(p, 9, "Help");
+
+            //Here opens the inventory
+            p.openInventory(trash);
+
+        }
+        else{
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',"&3このコマンドはコンソールから使用できません。" ));
+        }
+
+
+        return true;
     }
 }
