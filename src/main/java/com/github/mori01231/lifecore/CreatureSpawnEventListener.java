33aa7cd350d143925df41ee75d5f4506c38eeefb
Mori01231 <mori01231mc@gmail.com>
2020-07-16 22:38:06 +0900

    [Fix] Disabled logging completely due to console spam.

--- src/main/java/com/github/mori01231/lifecore/CreatureSpawnEventListener.java
+++ src/main/java/com/github/mori01231/lifecore/CreatureSpawnEventListener.java
@@ -21,18 +21,8 @@ public class CreatureSpawnEventListener implements Listener{
     @EventHandler(priority = EventPriority.NORMAL)
     public void onCreatureSpawn(CreatureSpawnEvent event) {
         if(event.getEntityType().equals(EntityType.BEE)){
-
-            // Combine the coordinates into one string
-            String originLocation = " x: " + (int)Math.round(Objects.requireNonNull(event.getEntity().getOrigin()).getX()) + " y: " + (int)Math.round(Objects.requireNonNull(event.getEntity().getOrigin()).getY()) + " z: " + (int)Math.round(Objects.requireNonNull(event.getEntity().getOrigin()).getZ()) + " ";
-
-            // Get the world name of the bee spawn
-            String originWorld = event.getEntity().getOrigin().getWorld().getName();
-
             // Remove the bee
             event.getEntity().remove();
-
-            // Log the location of the beehive
-            getLogger().info("ハチが湧いてきやがった、、、ふざけんな！" + originWorld + "ワールドの" + originLocation + "に巣があるので消しておいてくれ");
         }
 
     }
