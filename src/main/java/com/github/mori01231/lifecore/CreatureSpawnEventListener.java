465ea1db1c7aa2aba1b342003d84a86f85b1fb8d
Mori01231 <mori01231mc@gmail.com>
2020-07-16 22:38:30 +0900

    [Refactor] Removed unnecessary imports

--- src/main/java/com/github/mori01231/lifecore/CreatureSpawnEventListener.java
+++ src/main/java/com/github/mori01231/lifecore/CreatureSpawnEventListener.java
@@ -1,15 +1,10 @@
 package com.github.mori01231.lifecore;
 
-
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.Listener;
-
-import java.util.Objects;
-
-import static org.bukkit.Bukkit.getLogger;
 
 public class CreatureSpawnEventListener implements Listener{
 
