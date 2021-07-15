c21ab9b8b37c75bf0defe949ba97c0dbf55cbb70
Mori01231 <mori01231mc@gmail.com>
2021-07-15 21:23:38 +0900

    [Add] Resource command to LifeCore.java

--- src/main/java/com/github/mori01231/lifecore/LifeCore.java
+++ src/main/java/com/github/mori01231/lifecore/LifeCore.java
@@ -32,6 +32,7 @@ public final class LifeCore extends JavaPlugin {
         this.getCommand("pve1").setExecutor(new Pve1CommandExecutor());
         this.getCommand("pve2").setExecutor(new Pve2CommandExecutor());
         this.getCommand("life").setExecutor(new LifeCommandExecutor());
+        this.getCommand("resource").setExecutor(new ResourceCommandExecutor());
         this.getCommand("rank").setExecutor(new RankCommandExecutor());
         this.getCommand("sara").setExecutor(new SaraCommandExecutor());
         this.getCommand("trash").setExecutor(new TrashCommandExecutor());
